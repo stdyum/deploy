f2e9532c7c8535d4a0d8f2da10d5b74f8983c1d8
likdan <likdan.company@gmail.com>
2024-03-06 00:44:04 +0300

    remove sleep because of healthy check

--- up.sh
+++ up.sh
@@ -3,18 +3,10 @@
 docker compose -f services/database/docker-compose.yml up -d --remove-orphans
 docker compose -f services/kafka/docker-compose.yml up -d --remove-orphans
 
-echo "kafka and database were started, waiting for 30 seconds"
-sleep 30s
-
 docker compose -f apps/backend/docker-compose.yml up -d --remove-orphans
-
-echo "backend was started, waiting for 30 seconds"
-sleep 30s
-
 docker compose -f apps/frontend/docker-compose.yml up -d --remove-orphans
 
-echo "frontend was started, waiting for 30 seconds"
-sleep 30s
+sleep 5s
 
 echo "docker ps"
 docker ps
