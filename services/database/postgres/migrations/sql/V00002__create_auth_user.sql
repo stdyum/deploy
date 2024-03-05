f2a9058a21b0839356606be8b7be9a1279683192
likdan <likdan.company@gmail.com>
2024-03-05 23:48:17 +0300

    fix migrations

--- services/database/postgres/migrations/sql/V00002__create_auth_user.sql
+++ services/database/postgres/migrations/sql/V00002__create_auth_user.sql
@@ -2,4 +2,4 @@ CREATE USER auth WITH PASSWORD '${auth_password}';
 
 set search_path = "auth";
 GRANT ALL PRIVILEGES ON DATABASE auth TO auth;
-GRANT ALL ON SCHEMA auth.public TO auth;
+GRANT ALL ON SCHEMA public TO auth;
