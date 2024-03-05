1ad6977477923cd3049ee11856fc04540fc514bc
likdan <likdan.company@gmail.com>
2024-03-05 18:51:51 +0300

    env init script

--- scripts/env.setup.sh
+++ scripts/env.setup.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-files="services/database/postgres/.env" "services/database/postgres/migrations/flyway.conf" "services/database/redis/.env" "services/database/scylla/.env" "services/database/scylla/cassandra-rackdc.properties" "services/database/scylla/scylla.yaml" "services/database/scylla/migrations/flyway.conf" "services/kafka/configs/kafka_server.conf" "services/kafka/configs/ui.yaml" "apps/backend/auth/.env" "apps/backend/studyplaces/.env" "apps/backend/types_registry/.env"  "apps/backend/schedule/.env"  "apps/frontend/web/.env"
+files=("services/database/postgres/.env" "services/database/postgres/migrations/flyway.conf" "services/database/redis/.env" "services/database/scylla/.env" "services/database/scylla/cassandra-rackdc.properties" "services/database/scylla/scylla.yaml" "services/database/scylla/migrations/flyway.conf" "services/kafka/configs/kafka_server.conf" "services/kafka/configs/ui.yaml" "apps/backend/auth/.env" "apps/backend/studyplaces/.env" "apps/backend/types_registry/.env"  "apps/backend/schedule/.env"  "apps/frontend/web/.env")
 
 for file in "${files[@]}"
 do
   echo "Enter content for file: $file (press Control+D to finish input)"
-  touch "$file"
   cat > "$file"
 done
