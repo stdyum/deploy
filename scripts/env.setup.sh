96764b5f5693407ac80d80fce06048fdce077481
likdan <likdan.company@gmail.com>
2024-03-05 18:23:39 +0300

    env init script

--- scripts/env.setup.sh
+++ scripts/env.setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=("services/database/postgres/.env" "services/database/postgres/migrations/flyway.conf" "services/database/redis/.env" "services/database/scylla/.env" "services/database/scylla/cassandra-rackdc.properties" "services/database/scylla/scylla.yaml" "services/database/scylla/migrations/flyway.conf" "services/kafka/configs/kafka_server.conf" "services/kafka/configs/ui.yaml" "apps/backend/auth/.env" "apps/backend/studyplaces/.env" "apps/backend/types_registry/.env"  "apps/backend/schedule/.env"  "apps/frontend/web/.env")
+files="services/database/postgres/.env" "services/database/postgres/migrations/flyway.conf" "services/database/redis/.env" "services/database/scylla/.env" "services/database/scylla/cassandra-rackdc.properties" "services/database/scylla/scylla.yaml" "services/database/scylla/migrations/flyway.conf" "services/kafka/configs/kafka_server.conf" "services/kafka/configs/ui.yaml" "apps/backend/auth/.env" "apps/backend/studyplaces/.env" "apps/backend/types_registry/.env"  "apps/backend/schedule/.env"  "apps/frontend/web/.env"
 
 for file in "${files[@]}"
 do
