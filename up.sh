96f21c21cd744361057b989a76944607632f8e54
likdan <likdan.company@gmail.com>
2024-03-06 10:47:15 +0300

    remove sleep because of healthy check

--- up.sh
+++ up.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 docker compose -f services/database/docker-compose.yml up -d --remove-orphans
+docker compose -f services/database/docker-compose.yml logs
+
 docker compose -f services/kafka/docker-compose.yml up -d --remove-orphans
+docker compose -f services/kafka/docker-compose.yml logs
 
 docker compose -f apps/backend/docker-compose.yml up -d --remove-orphans
+docker compose -f apps/backend/docker-compose.yml logs
+
 docker compose -f apps/frontend/docker-compose.yml up -d --remove-orphans
+docker compose -f apps/frontend/docker-compose.yml up
 
 sleep 5s
 
