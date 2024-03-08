c21ac4ac2c94859fdcf7716313c9bc9b7ed3f505
likdan <likdan.company@gmail.com>
2024-03-09 01:18:13 +0300

    update

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ pwd=$(pwd)
 echo $pwd
 
 cd $pwd/apps/$1/$2 && git pull && docker build -t studyum-$2 -f deploy/Dockerfile .
-cd "$pwd/apps/$1" && docker compose stop && docker compose start
+cd "$pwd/apps/$1" && docker-compose stop $2 &&  docker-compose up -d --no-deps $2
